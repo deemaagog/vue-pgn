92a802f7a78fda884006d86a434b7b4286565fab
Дмитрий Дегтярев <1-003@vikiparts.lan>
2018-03-21 12:45:13 +0300

    notation, styling

--- src/components/Notation.vue
+++ src/components/Notation.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="notation">
-    <div class = "movePair"  v-for="(pair,pInd) in movePairs" :key=pInd>
-      <span>{{pInd + 1}}. </span>  
-      <span class = "move"
-      v-for="(move,mInd) in pair" 
-      :key=mInd
-      :class="{ current: currentMove === (pInd * 2 + mInd + 1)}"  
-      @click="goToMove(pInd * 2 + mInd + 1)"> {{move}} 
-      </span>
-      
+    <div class="moves">
+      <div class = "wrapper">
+        <template   v-for="(pair,pInd) in movePairs" >
+          <span class = "move"
+          v-for="(move,mInd) in pair" 
+          :key="`${pInd}_${mInd}`"
+          :class="{ current: currentMove === (pInd * 2 + mInd + 1)}"  
+          @click="goToMove(pInd * 2 + mInd + 1)">
+            <span v-if="mInd === 0">{{pInd + 1}}. </span>
+            {{move}}
+          </span>
+        </template>
+      </div>
+    </div>
+    <div class="panel">
+       <button class = "button" @click="goToMove(0)">&lt;&lt;</button>
+       <button class = "button" @click="goToMove(currentMove - 1)">&lt;</button>
+       <button class = "button" @click="goToMove(currentMove + 1)">&gt;</button>
+       <button class = "button" @click="goToMove(history.length)">&gt;&gt;</button> 
     </div>
   </div>
 </template>
@@ -26,7 +36,6 @@ export default {
       let p = [],
         copy = [...this.history];
       while (copy.length > 0) p.push(copy.splice(0, 2));
-      console.log(p)
       return p;
     }
   }
@@ -34,29 +43,18 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
 .notation {
   height: 400px;
-  width: 400px;
-  border: 1px solid;
+  /* width: 400px; */
+  width: 100%;
+  max-height: 400px;
+  display: flex;
+  flex-direction: column;
 }
 
 .move {
   cursor: pointer;
+  font-weight: bold;
 }
 
 .movePair {
@@ -64,6 +62,40 @@ a {
   margin-right: 4px;
 }
 .current {
-  font-weight: bold
+  background-color: rgb(230, 217, 144);
+}
+
+.moves {
+  letter-spacing: 1px;
+  text-align: left;
+  overflow: auto;
+  flex: 0 0 85%;
+}
+.button {
+  width: 40px;
+  height: 75%;
+  margin-left: 15px;
+  align-self: center;
+  cursor: pointer;
+  font-size: 14px;
+  border-radius: 4px;
+  background-color: #cecdcd;
+  touch-action: manipulation;
+  white-space: nowrap;
+  user-select: none;
+  border: 1px solid #bbbbbb;
+  text-decoration: none;
+  color: rgb(90, 90, 90);
+  font-weight: bold;
+}
+.wrapper {
+  padding: 10px;
+}
+
+.panel {
+  flex: 0 0 15%;
+  background-color: #f3f3f3;
+  display: flex;
+  justify-content: center;
 }
 </style>
